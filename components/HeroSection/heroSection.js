cdd609b658fab1b869b3cba4a3f7cee63a4ae9eb
Shrey Asthana <shreyasthana545@gmail.com>
2024-04-29 00:49:59 +0530

    apostrophie

--- components/HeroSection/heroSection.js
+++ components/HeroSection/heroSection.js
@@ -59,7 +59,7 @@ const HeroSection = () => {
         <div className={`${styles.heroContentText}`}>
             <div className={`${styles.title}`}>Onc Meds</div>
             <div className={`${styles.subTitle}`}><Type /></div>
-            <div className={`${styles.subTitle}`}>OncMeds is your one-stop shop for all things related to medicinal plants in India that have potential to fight cancer! This resource is packed with information about these plants, what medicinal properties they have, and how they might be used in cancer treatments. Whether you're a researcher, doctor, or just curious about plant-based options for cancer care, OncMeds can be your guide.</div>
+            <div className={`${styles.subTitle}`}>OncMeds is your one-stop shop for all things related to medicinal plants in India that have potential to fight cancer! This resource is packed with information about these plants, what medicinal properties they have, and how they might be used in cancer treatments. Whether you&apos;re a researcher, doctor, or just curious about plant-based options for cancer care, OncMeds can be your guide.</div>
             <div className={`${styles.subsubTitle}`}>Explore our comprehensive resource on Indian medicinal plants with potential benefits for cancer patients. We delve into both traditional Ayurvedic wisdom and the latest scientific findings, offering a one-stop shop for valuable insights. <br/></div>
         </div>
         <div className={`${styles.heroContentCta}`}>
